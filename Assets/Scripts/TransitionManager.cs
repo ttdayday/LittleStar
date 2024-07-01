54ee70a5e73b6eea7d15eefa820a309027dbedd6
tiantian ding <ttdayday@gmail.com>
2024-06-30 21:57:25 -0700

    0630  check in

--- Assets/Scripts/TransitionManager.cs
+++ Assets/Scripts/TransitionManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TTsTransitionScript : MonoBehaviour
+public class TransitionManager : MonoBehaviour
 {
     [SerializeField]
     private Transition[] transitions;
