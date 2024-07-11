49d65d527729063571e6f7d08ab7f4cc3872f085
tiantianttd <tidin@microsoft.com>
2024-07-10 21:26:29 -0700

    inventory

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -2,9 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu (fileName = "New Inventory", menuName = "Inventory System/Inventory")]
 public class Inventory: ScriptableObject
 {
-    public List<InventoryItem> Container = new List<InventoryItem>();
+    public List<InventorySlot> Container = new List<InventorySlot>();
+    public void AddItem(InventoryItem _item, int _amount)
+    {
+        bool hasItem = false;
+        for (int i = 0; i < Container.Count; i++)
+        {
+           if( Container[i].item == _item)
+           {
+                Container[i].AddAmount(_amount);
+                hasItem = true;
+                break; 
+            }                
+       }
+        if (!hasItem)
+        {
+            Container.Add(new InventorySlot(_item, _amount));
+
+        }
+    }
 
 
 }
@@ -15,7 +34,17 @@ public class InventorySlot
     public InventoryItem item;
     public int amount;
     public InventorySlot(InventoryItem _item, int _amount)
-    { }
+    {
+        item = _item;
+        amount = _amount;
+    }
+
+    public void AddAmount(int value)
+    {
+        amount += value;
+    }
+
+
 }
 
 
